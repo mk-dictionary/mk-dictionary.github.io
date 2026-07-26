1cdb9e24a33bd68ffa742f8513ea2c4783c323f9
rod520 <rodriguesorlen@gmail.com>
2026-07-26 08:17:04 -0500

    add toon

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,30 @@ import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// code borrowed from vite-plugin-toon, MIT license, subject to change
+import { decode } from '@toon-format/toon';
+/**
+ * Vite plugin for importing .toon files
+ * @param {Object} options - Plugin options
+ * @returns {import('vite').Plugin}
+ */
+function toonPlugin(options = {}) {
+	return {
+		name: 'vite-plugin-toon',
+		transform(src, id) {
+			if (id.endsWith('.toon')) {
+				const result = decode(src);
+				return {
+					code: `export default ${JSON.stringify(result)};`,
+					map: null
+				};
+			}
+		}
+	};
+}
+
+
+
 export default defineConfig({
 	plugins: [
 		sveltekit({
@@ -10,12 +34,13 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-
+				
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
-		})
+		}),
+		toonPlugin()
 	],
 	server: {
 		fs: {
