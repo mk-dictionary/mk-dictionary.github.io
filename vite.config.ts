3224bd120f40a408a270745b4e8772c627c815bd
rod520 <96319365+rod520@users.noreply.github.com>
2026-08-22 21:55:04 -0500

    deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ import { decode } from '@toon-format/toon';
 function toonPlugin() {
 	return {
 		name: 'vite-plugin-toon',
-		transform(src, id) {
+		transform(src: string, id: string) {
 			if (id.endsWith('.toon')) {
 				const result = decode(src);
 				return {
@@ -36,7 +36,10 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter({ strict: false })
+			adapter: adapter({ strict: false }),
+			paths: {
+    			base: process.env.NODE_ENV === "production" ? "/dictionary" : "",
+			},
 		}),
 		toonPlugin()
 	],
